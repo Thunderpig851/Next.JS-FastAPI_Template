379822ef85d1d2a0a3f8a03eceb53e8ccf170ee0
chad.szymarek <chadszymarek@gmail.com>
2022-10-19 21:04:40 -0600

    server container is running on docker

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -3,44 +3,51 @@ from typing import List
 from pydantic import BaseModel, EmailStr, constr
 from bson.objectid import ObjectId
 
+
 class UserBaseSchema(BaseModel):
     name: str
     email: str
-    photo: str | None = None
-    role: str | None = None
-    created_at: datetime | None = None
-    updated_at: datetime | None = None
+    photo: str
+    role: str
+    created_at: datetime
+    updated_at: datetime
 
     class Config:
         orm_model = True
+
 
 class CreateUserSchema(UserBaseSchema):
     password: constr(min_length=8)
     passwordConfirm: str
     verified: bool = True
 
+
 class LoginUserSchema(BaseModel):
     email: EmailStr
     password: constr(min_length=8)
+
 
 class UserResponseSchema(UserBaseSchema):
     id: str
     pass
 
+
 class UserResponse(BaseModel):
     status: str
     user: UserResponseSchema
 
+
 class FilteredUserResponse(UserBaseSchema):
     id: str
+
 
 class PostBaseSchema(BaseModel):
     title: str
     content: str
     category: str
     image: str
-    created_at: datetime | None = None
-    updated_at: datetime | None = None
+    created_at: datetime
+    updated_at: datetime
 
     class Config:
         orm_model = True
@@ -48,9 +55,11 @@ class PostBaseSchema(BaseModel):
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
 
+
 class CreatePostSchema(PostBaseSchema):
-    user: ObjectId | None = None
+    user: ObjectId
     pass
+
 
 class PostResponse(PostBaseSchema):
     id: str
@@ -58,18 +67,20 @@ class PostResponse(PostBaseSchema):
     created_at: datetime
     updated_at: datetime
 
+
 class UpdatePostSchema(BaseModel):
-    title: str | None = None
-    content: str | None = None
-    category: str | None = None
-    image: str | None = None
-    user: str| None = None
+    title: str
+    content: str
+    category: str
+    image: str
+    user: str
 
     class Config:
         orm_model = True
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
+
 
 class ListPostResponse(BaseModel):
     status: str
